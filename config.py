67f23eb240afbf40f43cc5543044b19a02148d5c
Jyothi Rupa S <jyothirupa.96@gmail.com>
2020-10-29 23:53:49 +0530

    creating twitter API instance using tweepy

---
+++ config.py
@@ -0,0 +1,14 @@
+### FILL IN THE DETAILS AFTER CREATING YOUR TWITTER DEVELOPER ACCOUNT ###
+
+# username - used to verify who you are on Twitter
+API_KEY = ''
+
+# password - identifies your account
+API_SECRET_KEY = ''
+
+# access token that allows authentication that allows you to pull specific data
+BEARER_TOKEN = ''
+
+ACCESS_TOKEN = ''
+
+ACCESS_TOKEN_SECRET = ''
